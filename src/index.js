1902c6605658475e40d63b4cea8aabfe30eb3ec7
Felix Höhne <felix.hoehne@akelius.de>
2020-02-14 15:29:19 +0100

    fixed removing too much stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Meals from "./meals/Meals.component";
-import Rating from "./rating/Rating.component";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/rating" component={Rating} />
-    </div>
-  </Router>
-);
+const routing = <App />;
 
 ReactDOM.render(routing, document.getElementById("root"));
